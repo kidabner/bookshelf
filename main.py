f3fa12952fe217b5a6f695dc270825b02644a013
kidabner <54899075+kidabner@users.noreply.github.com>
2022-11-19 18:02:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-﻿from flask import Flask,request,render_template
+from flask import Flask,request,render_template
 import os
 app = Flask(__name__)
 textList = os.listdir('static')
@@ -9,15 +9,15 @@ def home():
     data = ''
     if request.method == 'POST':
         book = request.form['book']
-        print(book)
+#         print(book)
         if book in textList:
             try:
-                file = open('static/'+book+'.txt','r',encoding='utf-8')
+                file = open('static/book/'+book+'.txt','r',encoding='utf-8')
                 data = file.read()
                 file.close()
             except:
-                file = open('static/'+book+'.txt','r',encoding='ANSI')
+                file = open('static/book/'+book+'.txt','r',encoding='ANSI')
                 data = file.read()
                 file.close()
-    return render_template('home.html',data=data,textList=textList)
-app.run('0.0.0.0',5000)
+    return render_template('bookshelf.html',data=data,textList=textList)
+app.run('0.0.0.0',5050)
