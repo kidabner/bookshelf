33630c51be22bdfae532ab90152ee3fe11371663
kidabner <54899075+kidabner@users.noreply.github.com>
2022-11-19 17:55:51 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+﻿from flask import Flask,request,render_template
+import os
+app = Flask(__name__)
+textList = os.listdir('static')
+textList = [i[:-4] for i in textList]
+@app.route('/',methods=['POST','GET'])
+def home():
+    global textList
+    data = ''
+    if request.method == 'POST':
+        book = request.form['book']
+        print(book)
+        if book in textList:
+            try:
+                file = open('static/'+book+'.txt','r',encoding='utf-8')
+                data = file.read()
+                file.close()
+            except:
+                file = open('static/'+book+'.txt','r',encoding='ANSI')
+                data = file.read()
+                file.close()
+    return render_template('home.html',data=data,textList=textList)
+app.run('0.0.0.0',5000)
