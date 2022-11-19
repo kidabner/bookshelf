7739e6fdbee4fce5c184120544a534499d14328a
kidabner <54899075+kidabner@users.noreply.github.com>
2022-11-19 18:39:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 from flask import Flask,request,render_template
 import os
+
 app = Flask(__name__)
-textList = os.listdir('static')
-textList = [i[:-4] for i in textList]
+
 @app.route('/',methods=['POST','GET'])
 def home():
-    global textList
+    # 遍历文件夹，获取所有的文件名，返回一个列表
+    bookList = os.listdir('static/book/')
     data = ''
     if request.method == 'POST':
+        # 提取传递进服务器的文件名
         book = request.form['book']
-#         print(book)
-        if book in textList:
-            try:
-                file = open('static/book/'+book+'.txt','r',encoding='utf-8')
-                data = file.read()
-                file.close()
-            except:
-                file = open('static/book/'+book+'.txt','r',encoding='ANSI')
-                data = file.read()
-                file.close()
-    return render_template('bookshelf.html',data=data,textList=textList)
-app.run('0.0.0.0',5050)
+        try:
+            file = open('static/book/'+book,'r',encoding='utf-8')
+            data = file.read()
+            file.close()
+        except:
+            file = open('static/book/'+book,'r',encoding='ANSI')
+            data = file.read()
+            file.close()
+    return render_template('bookshelf.html',data=data,bookList=bookList)
+app.run('0.0.0.0',8000)
